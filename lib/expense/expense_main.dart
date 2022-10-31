e78cfef724bd7496f75623d1c61f58aef798cfc8
Gopi M <gopi.learner93@gmail.com>
2022-10-31 20:38:09 +0530

    styling list container

--- lib/expense/expense_main.dart
+++ lib/expense/expense_main.dart
@@ -37,7 +37,7 @@ class _ExpenseAppState extends State<ExpenseApp> {
           children: [
             Container(
               width: double.infinity,
-              child: Card(
+              child: const Card(
                 elevation: 10,
                 color: Colors.blue,
                 child: Text('CHART'),
@@ -45,7 +45,50 @@ class _ExpenseAppState extends State<ExpenseApp> {
             ),
             Column(
               children: transactions.map((tx) {
-                return Card(child: Text(tx.title));
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
               }).toList(),
             )
           ],
