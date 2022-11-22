9fe191ddb2c0baa6b1e3dc7800c8821be49d2502
Gopi M <gopi.learner93@gmail.com>
2022-11-22 17:02:56 +0530

    favorites screen logic add

--- lib/meals/screens/favorites_screen.dart
+++ lib/meals/screens/favorites_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+import '../widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
+  final List<Meal> favriteMeals;
+
+  const FavoritesScreen(this.favriteMeals, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Favorites'),
-    );
+    if (favriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - start adding some!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: ((ctx, index) {
+          return MealItem(
+            id: favriteMeals[index].id,
+            title: favriteMeals[index].title,
+            imageUrl: favriteMeals[index].imageUrl,
+            duration: favriteMeals[index].duration,
+            complexity: favriteMeals[index].complexity,
+            affordability: favriteMeals[index].affordability,
+            // removeItem: _removeMeal,
+          );
+        }),
+        itemCount: favriteMeals.length,
+      );
+    }
   }
 }
