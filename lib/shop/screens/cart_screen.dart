6459ee9c452862451ec6699dfbe11ad57a569be7
Gopi M <gopi.learner93@gmail.com>
2022-11-29 21:28:41 +0530

    cart item list add

--- lib/shop/screens/cart_screen.dart
+++ lib/shop/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -38,13 +39,26 @@ class CartScreen extends StatelessWidget {
                   TextButton(
                     onPressed: (() {}),
                     child: Text('ORDER NOW'),
-                    // style: ButtonStyle(
-                    // ),
                   )
                 ],
               ),
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: ((ctx, i) => CartItem(
+                    cart.items.values.toList()[i].id,
+                    cart.items.values.toList()[i].price,
+                    cart.items.values.toList()[i].quantity,
+                    cart.items.values.toList()[i].title,
+                  )),
+              itemCount: cart.items.length,
+            ),
+          ),
+          // Expanded(child: )
         ],
       ),
     );
