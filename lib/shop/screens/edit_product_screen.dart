7cabb20257260f6e645e648b8da768076c95dd27
Gopi M <gopi.learner93@gmail.com>
2022-12-09 16:34:11 +0530

    add new product functionality add

--- lib/shop/screens/edit_product_screen.dart
+++ lib/shop/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -59,6 +61,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
     print(_editedProduct.price);
     print(_editedProduct.description);
     print(_editedProduct.imageUrl);
+
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override
--- lib/shop/screens/user_products_screen.dart
+++ lib/shop/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = 'user-products';
@@ -17,7 +18,9 @@ class UserProductsScreen extends StatelessWidget {
         title: const Text('Your Products'),
         actions: [
           IconButton(
-            onPressed: (() {}),
+            onPressed: (() {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            }),
             icon: const Icon(Icons.add),
           )
         ],
