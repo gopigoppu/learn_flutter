e73914c6622515e2c1cd582118062a19b2e4ac3f
Gopi M <gopi.learner93@gmail.com>
2022-12-19 15:06:38 +0530

    auth url format change

--- lib/shop/providers/auth.dart
+++ lib/shop/providers/auth.dart
@@ -7,17 +7,17 @@ class Auth with ChangeNotifier {
   late final String _token;
   late final DateTime _expiryDate;
   late final String _userId;
-  final String firebaseSignupUrl =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyArZkfV9Jlw2pQxsEH-WwwOQR6Rx5rKdEY';
-  final String firebaseLoginUrl =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArZkfV9Jlw2pQxsEH-WwwOQR6Rx5rKdEY';
+
+  final String firebaseUrl =
+      'https://identitytoolkit.googleapis.com/v1/accounts:';
+  final String projK = 'AIzaSyArZkfV9Jlw2pQxsEH-WwwOQR6Rx5rKdEY';
 
   // Auth(this._token, this._expiryDate, this._userId);
 
   Future<void> _authenticate(
-      String email, String password, String authUrl) async {
+      String email, String password, String segment) async {
     try {
-      final url = Uri.parse(authUrl);
+      final url = Uri.parse('${firebaseUrl}${segment}?key=${projK}');
       final response = await http.post(
         url,
         body: json.encode(
@@ -35,10 +35,10 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> signup(String email, String password) async {
-    return _authenticate(email, password, firebaseSignupUrl);
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    return _authenticate(email, password, firebaseLoginUrl);
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
